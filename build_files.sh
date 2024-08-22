a85cb55d592ebd892a8fff22ee851620315bd3a0
Enrique Ferreiro <ztazhorde@gmail.com>
2024-08-22 00:51:10 -0400

    Arreglando el build

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,5 @@
 # build_files.sh
-which python3.9
-pwd
-cd /usr/local/bin/
-yum install sqlite-devel -y
-./configure --enable-optimizations --enable-loadable-sqlite-extensions
-make && make altinstall
-cd /vercel/path0/
+python3.9 -m venv venv
+source venv/bin/activate
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
